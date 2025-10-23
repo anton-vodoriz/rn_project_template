2c1e07d84aba6c0fecb7a302d045339857aa86ab
monInspiration <anton.vodoriz@gmail.com>
2025-10-23 12:31:30 +0200

    template config fixes

--- template.config.js
+++ template.config.js
@@ -1,6 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
-  placeholderName: "MyApp",
-  titlePlaceholder: "My React Native App",
-  templateDir: "./template",
-  postInitScript: "./script.js",
+  // This function is called after the template is copied
+  postInstall: async ({ projectRoot, appName }) => {
+    console.log(`Running post-install script for ${appName}...`);
+
+    // Files to replace placeholders
+    const filesToPatch = ['app.json', 'package.json'];
+
+    filesToPatch.forEach((file) => {
+      const filePath = path.join(projectRoot, file);
+      if (!fs.existsSync(filePath)) return;
+
+      let content = fs.readFileSync(filePath, 'utf8');
+      content = content.replace(/__APP_NAME__/g, appName);
+      fs.writeFileSync(filePath, content, 'utf8');
+    });
+
+    console.log(`App name "${appName}" inserted successfully!`);
+  },
+
+  // Optional: if you want to skip some dependencies installation
+  // installDependencies: true,
 };
